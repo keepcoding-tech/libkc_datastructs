ca41ea41f4609d935cf0007b747a461a158c3f04
Daniel Tanase <tanasedaniel54@gmail.com>
2023-07-27 18:50:57 +0300

    feat: update stack funcs

--- tests/stack.c
+++ tests/stack.c
@@ -8,6 +8,49 @@
 void test_creation_and_destruction() {
   struct Stack* stack = new_stack();
   assert(stack->vector->data[0] == NULL);
+  assert(stack->length(stack) == 0);
+  assert(stack->vector->capacity == 16);
+  destroy_stack(stack);
+}
+
+// Test case for the length() method.
+void test_length() {
+  struct Stack* stack = new_stack();
+
+  // the length should be zero
+  assert(stack->length(stack) == 0);
+
+  // push 10 more items
+  for (int i = 0; i < 10; ++i) {
+    stack->push(stack, &i, sizeof(int));
+  }
+
+  assert(stack->length(stack) == 10);
+
+  stack->vector->clear(stack->vector);
+
+  // should be empty again
+  assert(stack->length(stack) == 0);
+
+  destroy_stack(stack);
+}
+
+// Test case for the pop() method.
+void test_pop() {
+  struct Stack* stack = new_stack();
+
+  // push 10 new nodes
+  for (int i = 0; i < 10; ++i) {
+    stack->push(stack, &i, sizeof(i));
+  }
+
+  for (int i = 1; i < 10; ++i) {
+    stack->pop(stack);
+
+    // check if the items have been removed correctly
+    assert(stack->vector->length == 10 - i);
+  }
+
   destroy_stack(stack);
 }
 
@@ -31,30 +74,30 @@ void test_push() {
   destroy_stack(stack);
 }
 
-// Test case for the pop() method.
-void test_pop() {
+// Test case for the top() method.
+void test_top() {
   struct Stack* stack = new_stack();
 
-  // push ten new nodes (if the test reached here then the "push"
-  // function was successfully tested, so we can freely use it)
-  for (int i = 0; i < 10; ++i) {
-    stack->push(stack, &i, sizeof(i));
+  // push 60 more items
+  for (int i = 0; i < 60; ++i) {
+    stack->push(stack, &i, sizeof(int));
+
+    // check each new item
+    assert(*(int*)stack->top(stack) == i);
   }
 
-  for (int i = 1; i < 10; ++i) {
-    stack->pop(stack);
-
-    // check if the items have been removed correctly
-    assert(stack->vector->length == 10 - i);
-  }
+  // check the capacity of the vector
+  assert(stack->vector->capacity == 64);
 
   destroy_stack(stack);
 }
 
 int main() {
   test_creation_and_destruction();
-  test_push();
+  test_length();
   test_pop();
+  test_push();
+  test_top();
   printf("stack.t .................... OK\n");
   return 0;
 }
